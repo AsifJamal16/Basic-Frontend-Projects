8dcc10536d9f7c629bfbb6626294a0e669747889
Asif Jamal <asifjamal582005@gmail.com>
2026-01-16 14:10:15 +0000

    togglepage

---
+++ togglePage/index.js
@@ -0,0 +1,46 @@
+
+        let webpage=document.querySelector('body');
+        let blue=document.querySelector('#blue');
+        blue.addEventListener('click',function(){
+            webpage.style.backgroundColor='#007bff';
+        })
+        let green=document.querySelector('#green');
+        green.addEventListener('click',function(){
+            webpage.style.backgroundColor='#28a745';
+        })
+        let red=document.querySelector('#Red');
+        red.addEventListener('click',function(){
+            webpage.style.backgroundColor='#dc3545';
+        })
+        let yellow=document.querySelector('#Yellow');
+        yellow.addEventListener('click',function(){
+            webpage.style.backgroundColor='#ffc107';
+        })
+        let orange=document.querySelector('#orange');
+        orange.addEventListener('click',function(){
+            webpage.style.backgroundColor='#fd7e14';
+        })
+        let purple=document.querySelector('#purple');
+        purple.addEventListener('click',function(){
+            webpage.style.backgroundColor='#6f42c1';
+        })
+        let teal=document.querySelector('#teal');
+        teal.addEventListener('click',function(){
+            webpage.style.backgroundColor='#20c997';
+        })
+        let cyan=document.querySelector('#cyan');
+        cyan.addEventListener('click',function(){
+            webpage.style.backgroundColor='#17a2b8';
+        })
+        let pink=document.querySelector('#pink');
+        pink.addEventListener('click',function(){
+            webpage.style.backgroundColor='#e83e8c';
+        })
+        let indigo=document.querySelector('#indigo');
+        indigo.addEventListener('click',function(){
+            webpage.style.backgroundColor='#6610f2';
+        })
+        let gray=document.querySelector('#gray');
+        gray.addEventListener('click',function(){
+            webpage.style.backgroundColor='#343a40';
+        })
